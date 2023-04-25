72126990a28b0f9c655b1a56e7417ac1ebb1d5c0
D1manko <d.stamatov@yahoo.com>
2023-04-25 20:37:52 +0300

    Implemented MergeLists

--- LinearDataStructures exercise/Template/LinearDataStructures/LinearStructuresTasks.cs
+++ LinearDataStructures exercise/Template/LinearDataStructures/LinearStructuresTasks.cs
@@ -54,21 +54,75 @@ namespace LinearDataStructures
         }
 
         public static SinglyLinkedList<T> MergeLists<T>(SinglyLinkedList<T> list1, SinglyLinkedList<T> list2) where T : IComparable
-        {
-            
-            var mergedList = new SinglyLinkedList<T>();
-            var currentNodeOne = list1.Head;
-            var currentNodeTwo = list2.Head;
+        {            
+            var mergedList = new SinglyLinkedList<T>();            
             int listOneLength = GetListLength(list1);
             int listTwoLength = GetListLength(list2);
-            
+            if (listOneLength == 0 && listTwoLength == 0)
+            {
+                return mergedList;
+            }
+            else if (listOneLength == 0)
+            {
+                return list2;
+            }
+            else if (listTwoLength == 0)
+            {
+                return list1;
+            }
+            while (list1.Head != null && list2.Head != null)
+            {
+                var valueOne = list1.Head.Value;
+                var valueTwo = list2.Head.Value;
+
+                if (valueOne.CompareTo(valueTwo) < 0)
+                {
+                    mergedList.AddFirst(valueOne);
+                    
+                    list1.RemoveFirst();
+                }
+                else
+                {
+                    mergedList.AddFirst(valueTwo);
+                    Console.WriteLine(valueTwo);
+                    list2.RemoveFirst();
+
+                }
+            }
+
+            if (list1.Head == null)
+            {
+                while (list2.Head != null)
+                {
+
+                    mergedList.AddFirst(list2.RemoveFirst());
+                }
+            }
+            else
+            {
+                while (list1.Head != null)
+                {
+
+                    mergedList.AddFirst(list1.RemoveFirst());
+                }
+            }
+
+            return ReverseList(mergedList);
 
         }
 
         public static SinglyLinkedList<T> ReverseList<T>(SinglyLinkedList<T> list)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            var reversedList = new SinglyLinkedList<T>();
+            int elementsCount = GetListLength(list);
+            for (int index = 0; index < elementsCount; index++)
+            {
+                
+                reversedList.AddFirst(list.RemoveFirst());                
+            }
+
+            return reversedList;
+            
         }
 
         public static bool AreValidParentheses(string expression)
@@ -82,7 +136,7 @@ namespace LinearDataStructures
             // Add your implementation here.
             throw new NotImplementedException();
         }
-        private static int GetListLength<T>(SinglyLinkedList<T> list)
+        public static int GetListLength<T>(SinglyLinkedList<T> list)
         {
             
             if (list == null || list.Head == null)
@@ -102,7 +156,7 @@ namespace LinearDataStructures
         {
             return EqualityComparer<T>.Default.Equals(x, y);
         }
-        private static void ValidateEmptyList<T>(SinglyLinkedList<T> list)
+        public static void ValidateEmptyList<T>(SinglyLinkedList<T> list)
         {
             int elementsCount = GetListLength(list);
             if (elementsCount == 0)
--- LinearDataStructures exercise/Template/LinearDataStructures/Program.cs
+++ LinearDataStructures exercise/Template/LinearDataStructures/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LinearDataStructures.Common;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using static LinearDataStructures.LinearStructuresTasks;
 
 namespace LinearDataStructures
 {
@@ -6,7 +9,19 @@ namespace LinearDataStructures
     {
         static void Main()
         {
+            var testList = new int[] { 1, 2, 3 };
+            var testList2 = new int[] { 4, 5, 6 };
+            var inputList = new SinglyLinkedList<int>(testList);
+            var inputList2 = new SinglyLinkedList<int>(testList2);
+            var newList = LinearStructuresTasks.MergeLists(inputList, inputList2);
+            int listLength = GetListLength(newList);
             
+            for (int index = 0; index < listLength; index++)
+            {
+                
+                Console.WriteLine(newList.RemoveFirst());
+                
+            }
         }
     }
 }
