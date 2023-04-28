837952e6937d9426111ac59f22865439caa7eba2
Diman_Stamatov <d.stamatov@yahoo.com>
2023-04-28 13:35:04 +0300

    Optimised StudentsOrder

--- StudentsOrder/StudentsOrder/Program.cs
+++ StudentsOrder/StudentsOrder/Program.cs
@@ -3,43 +3,48 @@ using System.Linq;
 using System.Collections.Generic;
 
 
+using System.Text;
+
 namespace StudentsOrder
 {
+    
     internal class Program
     {
+        
 
         static void Main(string[] args)
         {
             int[] initialInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int studentsCount = initialInput[0];
             int seatChanges = initialInput[1];
-            var dictionary = new Dictionary<string, string>();
-            dictionary.Add("misho", "gosho");
-            dictionary.Add("gosho", "penka");
-            var byKeyvalue = dictionary.First(pair => pair.Key == "misho");
-            var pair = new dicti byKeyvalue.Value = "pesho";
-            var students = Console.ReadLine().Split(' ').ToArray();
-            int keys = 1;
-            var items = from pair in dictionary
-            LinkedListNode<string> previousNode = null;
-            foreach (var item in students)
+            var dictionary = new Dictionary<string, LinkedListNode<string>>();
+            var list = new LinkedList<string>();
+            var students = Console.ReadLine().Split(' ');
+            
+            for (int index = 0; index < studentsCount; index++)
             {
-                var node = new LinkedListNode<string>(item);
-                node.Previous = previousNode;
-                dictionary.Add(keys, node);
+                string studentName = students[index];
+                
+                list.AddLast(studentName);                               
+                dictionary.Add(studentName, list.Last);
+                
             }
-            for (int seatChange = 0; seatChange < seatChanges; seatChange++)
+            for (int change = 0; change < seatChanges; change++)
             {
-                var newSeating = Console.ReadLine().Split(' ');
-                string studentOne = newSeating[0];
-                string studentTwo = newSeating[1];
-                var moveBeforeNode = students.Find(studentTwo);
-                students.Remove(studentOne);
-                var studentOneNode = new LinkedListNode<string>(studentOne);
-                students.AddBefore(moveBeforeNode, studentOneNode);
+                var seatChange = Console.ReadLine().Split(' ');
+                string studentMoving = seatChange[0];
+                string moveTo = seatChange[1];
 
+                list.Remove(dictionary[studentMoving]);
+                dictionary.Remove(studentMoving);
+                
+                var node = list.AddBefore(dictionary[moveTo], studentMoving);
+                dictionary.Add(studentMoving, node);
+                
             }
-            Console.WriteLine(string.Join(" ", students));
+            Console.WriteLine(String.Join(" ", list));
+            
         }
+        
     }
 }
