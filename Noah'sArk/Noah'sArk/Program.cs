fa01df000a8c6bd415c2d6074e6e53ab31b6e326
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-03 15:35:14 +0300

    Solved Noah's Ark

---
+++ Noah'sArk/Noah'sArk/Program.cs
@@ -0,0 +1,44 @@
+﻿
+ using System;
+ using System.Collections.Generic;
+
+
+namespace Noah_sArk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+            var animals = new Dictionary<string, int>();
+            for (int input = 0; input < inputs; input++)
+            {
+                string animal = Console.ReadLine();
+                if (animals.ContainsKey(animal) == false)
+                {
+                    animals.Add(animal, 1);
+                }
+                else
+                {
+                    animals[animal]++;
+                }
+            }
+            var list = new SortedList<string, int>(animals);
+            foreach (var animal in list)
+            {
+                string species = animal.Key;
+                int number = animal.Value;
+                string even;
+                    if (number % 2 == 0)
+                {
+                    even = "Yes";
+                }
+                else
+                {
+                    even = "No";
+                }
+                Console.WriteLine(species + " " + number + " " + even);
+            }
+        }
+    }
+}
