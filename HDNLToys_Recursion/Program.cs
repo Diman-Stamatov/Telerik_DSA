8ed815689dc6c20001bf9e296091a413ddbdef8c
D1manko <d.stamatov@yahoo.com>
2023-05-04 23:35:58 +0300

    Optimized GottaCatchEmAll

--- HDNLToys_Recursion/Program.cs
+++ HDNLToys_Recursion/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.ComponentModel;
+using static GottaCatchEmAll.Program;
 
 
 namespace GottaCatchEmAll
@@ -15,44 +15,49 @@ namespace GottaCatchEmAll
 
     internal class Program
     {
-        public class Pokemon:IComparable<Pokemon>
 
-
+        public class MyComparer : IComparer<Pokemon>
+        {
+            public int Compare(Pokemon x, Pokemon y)
+            {
+                if (x.Position < y.Position)
+                {
+                    return 1;
+                }
+                else if (x.Position == y.Position)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+        public class Pokemon
         {
             public string Name { get; set; }
             public string Type { get; set; }
             public int Power { get; set; }
+            public int Position { get; set; }
 
 
-
-            public Pokemon(string name, string type, int power)
+            public Pokemon(string name, string type, int power, int position)
             {
                 this.Name = name;
                 this.Type = type;
                 this.Power = power;
-
+                this.Position = position;
             }
 
             public override string ToString()
             {
                 return $"{this.Name}({this.Power})";
             }
-
-            int IComparable<Pokemon>.CompareTo(Pokemon other)
-            {
-               var result = this.Name.CompareTo(other.Name);
-                if (result==0)
-                {
-                    result = other.Power.CompareTo(this.Power);
-                }
-                return result;
-            }
         }
-        static Dictionary<string, SortedSet<Pokemon>> typeDictionary = new Dictionary<string, SortedSet<Pokemon>>();
-        static List<Pokemon> pokemons = new List<Pokemon>();
-
+        static Dictionary<string, SortedSet<Pokemon>> typeDictionary2 = new Dictionary<string, SortedSet<Pokemon>>();
         static StringBuilder output = new StringBuilder();
-        
+        static List<Pokemon> leaderBoard = new List<Pokemon>();
 
         static void Main(string[] args)
         {
@@ -86,22 +91,15 @@ namespace GottaCatchEmAll
             string type = input[2];
             int power = int.Parse(input[3]);
             int position = int.Parse(input[4]);
-            var newMon = new Pokemon(name, type, power);
-            if (position != pokemons.Count + 1)
-            {
-                pokemons.Insert(position - 1, newMon);
-            }
-            else
-            {
-                pokemons.Add(newMon);
-            }
+            var newMon = new Pokemon(name, type, power, position);
 
+            leaderBoard.Insert(position - 1, newMon);
 
-            if (!typeDictionary.ContainsKey(type))
+            if (!typeDictionary2.ContainsKey(type))
             {
-                typeDictionary.Add(type, new SortedSet<Pokemon>());
+                typeDictionary2.Add(type, new SortedSet<Pokemon>(new MyComparer()));
             }
-            typeDictionary[type].Add(newMon);
+            typeDictionary2[type].Add(newMon);
 
             output.Append($"Added pokemon {name} to position {position}");
             output.AppendLine();
@@ -110,18 +108,13 @@ namespace GottaCatchEmAll
         {
             string type = input[1];
             output.Append($"Type {type}: ");
-            if (!typeDictionary.ContainsKey(type))
+            if (typeDictionary2.ContainsKey(type))
             {
-                
-                output.AppendLine();
-                return;
+                output.Append(string.Join("; ", typeDictionary2[type]                    
+                    .OrderBy(mon => mon.Name).ThenByDescending(mon => mon.Power).Take(5)));
+
             }
-            
-
-
-            output.Append(string.Join("; ", typeDictionary[type].Take(5)));
             output.AppendLine();
-            
 
         }
         public static void Ranklist(string[] input)
@@ -129,16 +122,11 @@ namespace GottaCatchEmAll
             int startRank = int.Parse(input[1]);
             int endRank = int.Parse(input[2]);
 
-
-
-            for (int index = startRank - 1; index < endRank; index++)
+            for (int index = startRank - 1; index <= endRank - 1; index++)
             {
-                output.Append($"{index + 1}. {pokemons[index]}");
-                if (index != endRank - 1)
-                {
-                    output.Append("; ");
-                }
+                output.Append($"{index + 1}. {leaderBoard[index].ToString()}; ");
             }
+            output.Remove(output.Length - 2, 2);
             output.AppendLine();
 
         }
--- Workshop 1. Stack and Queue/Template/StackQueueWorkshop/Program.cs
+++ Workshop 1. Stack and Queue/Template/StackQueueWorkshop/Program.cs
@@ -1,12 +1,24 @@
-﻿using StackQueueWorkshop.Queue;
-using StackQueueWorkshop.Stack;
-using System;
-using System.Linq;
-
-class Program
+﻿using System;
+class GFG
 {
-    static void Main(string[] args)
+    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
     {
-       
+        if (n == 0)
+        {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        Console.WriteLine("Move disk " + n + " from rod "
+                          + from_rod + " to rod " + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+    // 32
+    //  Driver method
+    public static void Main(String[] args)
+    {
+        int N = 3;
+
+        // A, B and C are names of rods
+        towerOfHanoi(N, 'A', 'C', 'B');
     }
 }
