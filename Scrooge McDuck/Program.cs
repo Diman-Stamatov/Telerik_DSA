ee88b529c7cfd35020a7ed5fdeef1e49942b0225
D1manko <d.stamatov@yahoo.com>
2023-05-06 13:57:57 +0300

    Started working on ScrougeMcDuck

---
+++ Scrooge McDuck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrooge_McDuck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[,] vault = new int[rows, cols];
+            int startRow = 0;
+            int startCol = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    vault[row, col] = values[col];
+                    if (values[col] == 0)
+                    {
+                        startRow = row;
+                        startCol = col;
+                    }
+                }
+            }
+            
+        }
+    }
+}
