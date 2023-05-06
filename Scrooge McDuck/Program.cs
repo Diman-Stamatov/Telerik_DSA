8ce36b7490dc95253a7825db919bfcef440b1313
D1manko <d.stamatov@yahoo.com>
2023-05-06 15:32:20 +0300

    Finished ScrougeMcDuck

--- Scrooge McDuck/Program.cs
+++ Scrooge McDuck/Program.cs
@@ -8,6 +8,7 @@ namespace Scrooge_McDuck
 {
     internal class Program
     {
+        static int coins = 0;
         static void Main(string[] args)
         {
             var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
@@ -29,7 +30,85 @@ namespace Scrooge_McDuck
                     }
                 }
             }
+            Explore(vault, startRow, startCol);
+            Console.WriteLine(coins);
+
+        }
+        static void Explore(int[,] vault, int row, int col)
+        {
+            if (vault[row, col] != 0)
+            {
+                coins++;
+                vault[row, col]--;
+            }
+
+            bool leftInvalid = col - 1 < 0 || vault[row, col - 1] == 0;
+            bool rightInvalid = col + 1 >= vault.GetLength(1) || vault[row, col + 1] == 0;
+            bool upInvalid = row - 1 < 0 || vault[row-1, col] == 0;
+            bool downInvalid = row + 1 >= vault.GetLength(0) || vault[row+1, col] == 0;
+
+            if (leftInvalid && rightInvalid && upInvalid && downInvalid)
+            {
+                return;
+            }
+
             
+            int highestCoins = 0;
+            int left = 0;
+            int right = 0;
+            int up = 0;
+            int down = 0;
+
+            if (!leftInvalid)
+            {
+                left = vault[row,col-1];
+                if (left > highestCoins)
+                {
+                    highestCoins = left;    
+                }
+            }
+            if (!rightInvalid) 
+            { 
+                right = vault[row,col+1];
+                if (right > highestCoins)
+                {
+                    highestCoins = right;
+                }
+            }
+            if (!upInvalid) 
+            { 
+                up = vault[row-1,col];
+                if (up > highestCoins)
+                {
+                    highestCoins = up;
+                }
+            }
+            if (!downInvalid)
+            {
+                down = vault[row + 1, col];
+                if (down > highestCoins)
+                {
+                    highestCoins = down;
+                }
+            }
+            
+            if (highestCoins == left)
+            {
+                Explore(vault, row, col - 1);
+            }
+            else if (highestCoins == right) 
+            {
+                Explore(vault, row, col + 1);
+            }
+            else if (highestCoins == up)
+            {
+                Explore(vault, row-1, col);
+            }
+            else 
+            {
+                Explore(vault, row + 1, col);
+            }
+
         }
     }
 }
