21dbb77f5f058aa855c09e2784d9218d7d1ed5b8
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-09 17:08:01 +0300

    Finished Set tasks.

--- SetMapHashInHome/01. Set/Template/Program.cs
+++ SetMapHashInHome/01. Set/Template/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InHomeActivity.Set
 {
@@ -7,31 +8,72 @@ namespace InHomeActivity.Set
     {
         static void Main()
         {
+            var collection1 = new int[] { 1, 3, 5, 7, 9 };
+            var collection2 = new int[] { 2, 3, 4, 6 };
+            var difference = Difference(collection1, collection2);
+            Console.WriteLine(string.Join(',', difference)); // 1,5,7,9
+
+
+
         }
 
         static bool AreAllElementsUnique<T>(ICollection<T> collection)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<T>();
+            foreach (var item in collection)
+            {
+                if (!hashSet.Add(item))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
         static bool AreAllElementsUnique<T>(IEnumerable<T> collection)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<T>(collection);
+            if (hashSet.Count != collection.Count())
+            {
+                return false;
+            }
+            return true;
         }
         static IEnumerable<T> Distinct<T>(IEnumerable<T> collection)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<T>(collection);
+            return hashSet.ToList();
         }
         static IEnumerable<T> Union<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<T>(collection1);
+            hashSet.UnionWith(collection2);
+            return hashSet.ToList();
         }
         static IEnumerable<T> Intersect<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            var hashSet = new HashSet<T>(collection1);
+            var hashSetTwo = new HashSet<T>(collection2);
+            foreach (var item in hashSet)
+            {
+                if (!hashSetTwo.Contains(item))
+                {
+                    hashSet.Remove(item);
+                }
+            }
+            return hashSet.ToList();
         }
         static IEnumerable<T> Difference<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            var hashOne = new HashSet<T>(collection1);
+            var hashTwo = new HashSet<T>(collection2);
+            foreach (var item in hashOne)
+            {
+                if (hashTwo.Contains(item))
+                {
+                    hashOne.Remove(item);
+                }
+            }
+            return hashOne.ToList();
         }
     }
 }
