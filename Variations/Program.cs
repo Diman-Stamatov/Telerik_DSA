54c47c8769700317f5df3030876c8c50b87b19d8
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-10 16:57:10 +0300

    Started working on Variations

---
+++ Variations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int stringLength = int.Parse(Console.ReadLine());
+        List<char> input = Console.ReadLine().Split().ToList();
+
+        string[] variations = GenerateVariations(symbols, stringLength);
+
+        foreach (string variation in variations)
+        {
+            Console.WriteLine(variation);
+        }
+    }
+
+    static string[] GenerateVariations(string[] symbols, int length)
+    {
+        string[] variations = new string[(int)Math.Pow(symbols.Length, length)];
+
+        for (int i = 0; i < variations.Length; i++)
+        {
+            string variation = "";
+            int remainder = i;
+
+            for (int j = 0; j < length; j++)
+            {
+                int index = remainder % symbols.Length;
+                variation = symbols[index] + variation;
+                remainder /= symbols.Length;
+            }
+
+            variations[i] = variation;
+        }
+
+        var output = variations.OrderByDescending(x=>x).ToArray();
+        return output;
+    }
+}
