2f3555faa0f11e200073829158b5ec5ee2e3351e
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-10 17:05:06 +0300

    Still working on the Variations

--- Variations/Program.cs
+++ Variations/Program.cs
@@ -5,36 +5,46 @@ class Program
     static void Main(string[] args)
     {
         int stringLength = int.Parse(Console.ReadLine());
-        List<char> input = Console.ReadLine().Split().ToList();
-
-        string[] variations = GenerateVariations(symbols, stringLength);
-
-        foreach (string variation in variations)
-        {
-            Console.WriteLine(variation);
-        }
+        string input = Console.ReadLine();
+        char[] symbols = new char[] { input[0], input[2] };
+        int numberOfSymbols = 2;
+        printAllKLengthRec(symbols, "", numberOfSymbols, stringLength);
     }
 
-    static string[] GenerateVariations(string[] symbols, int length)
+    
+    // The main recursive method
+    // to print all possible
+    // strings of length k
+    static void printAllKLengthRec(char[] symbols, string prefix, int numberOfSymbols, int stringLength)
     {
-        string[] variations = new string[(int)Math.Pow(symbols.Length, length)];
 
-        for (int i = 0; i < variations.Length; i++)
+        // Base case: k is 0,
+        // print prefix
+        if (stringLength == 0)
         {
-            string variation = "";
-            int remainder = i;
-
-            for (int j = 0; j < length; j++)
-            {
-                int index = remainder % symbols.Length;
-                variation = symbols[index] + variation;
-                remainder /= symbols.Length;
-            }
-
-            variations[i] = variation;
+            Console.WriteLine(prefix);
+            return;
         }
 
-        var output = variations.OrderByDescending(x=>x).ToArray();
-        return output;
+        // One by one add all characters
+        // from set and recursively
+        // call for k equals to k-1
+        for (int i = 0; i < numberOfSymbols; ++i)
+        {
+
+            // Next character of input added
+            string newPrefix = prefix + symbols[i];
+
+            // k is decreased, because
+            // we have added a new character
+            printAllKLengthRec(symbols, newPrefix, numberOfSymbols, stringLength - 1);
+        }
     }
+
+    // Driver Code
+   
 }
+
+// This code is contributed by Ajit.
+
+
