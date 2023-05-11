25b8010d258fa058700a82938eefc34ea0045138
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-11 16:28:09 +0300

    Implemented CafeOrderChecker

---
+++ SearchingSortingInClass/Template/CafeOrderChecker/CafeOrderChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CafeOrderChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var takeout = new int[] { 1, 3, 5 };
+            var dineIn = new int[] { 2, 4, 6 };
+            var checkout = new int[] { 1, 2, 3, 5, 4, 6 };
+            Console.WriteLine(ValidOrders(takeout, dineIn, checkout));
+        }
+        static bool ValidOrders(int[] takeout, int[] dineIn, int[] checkout)
+        {
+            var combinedOrders = new int[takeout.Length + dineIn.Length];
+            Array.Copy(takeout, combinedOrders, takeout.Length);
+            Array.Copy(dineIn, 0, combinedOrders, takeout.Length, dineIn.Length);
+            Array.Sort(combinedOrders);
+            for (int index = 0; index < takeout.Length; index++)
+            {
+                if (combinedOrders[index] != checkout[index])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
