f510e57cf0dbe21dc3e1e1f73463ee3869e63c20
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-11 17:20:12 +0300

    Implemented InflightEntertainment

---
+++ SearchingSortingInClass/Template/InflightEntertainment/InflightEntertainment/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InflightEntertainment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int flighLength = 328;
+            int[] movies = new int[] { 123, 111, 94, 99, 230, 150, 128, 200 };
+
+            Console.WriteLine(FitTwoMovies(flighLength, movies));
+        }
+        static bool FitTwoMovies(int flightLength, int[] movies)
+        {
+            var hashset = new HashSet<int>(movies);
+            foreach (var item in hashset)
+            {
+                int secondRuntime = flightLength - item;
+                if (hashset.Contains(secondRuntime))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+    }
+}
