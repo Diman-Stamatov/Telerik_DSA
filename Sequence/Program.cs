0816bbc9dd294cada8d4b671fef9472097858549
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-16 10:51:11 +0300

    Somewhat implemented Sequence

---
+++ Sequence/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ');
+            int firstNumber = int.Parse(numbers[0]);
+            int nthNumber = int.Parse(numbers[1]);
+            Console.WriteLine(findNumber(firstNumber, nthNumber));
+
+        }
+        static int findNumber(int firstNumber, int nthNumber)
+        {
+            if (nthNumber == 1)
+            {
+                return firstNumber;
+            }
+            else if (nthNumber == 2)
+            {
+                return firstNumber + 1;
+            }
+            else if (nthNumber == 3)
+            {
+                return firstNumber * 2 + 1;
+            }
+            else if (nthNumber == 4)
+            {
+                return firstNumber + 2;
+            }
+            return findNumber(firstNumber+1, nthNumber - 3);
+        }
+    }
+}
