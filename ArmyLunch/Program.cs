9eb8423eb9a405a3aed9f5d7c90f6c1102443088
Diman_Stamatov <d.stamatov@yahoo.com>
2023-05-16 10:16:58 +0300

    Finished ArmyLunch

---
+++ ArmyLunch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArmyLunch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSoldiers = int.Parse(Console.ReadLine());
+            var soldiers = Console.ReadLine().Split(' ');
+            var chowOrder = new Dictionary<string, List<string>>();
+            foreach (var soldier in soldiers)
+            {
+                char soldierRank = soldier[0];
+                if (soldierRank == 'S')
+                {
+                    if (!chowOrder.ContainsKey("Sergeant"))
+                    {
+                        chowOrder.Add("Sergeant", new List<string>());
+                    }
+                    chowOrder["Sergeant"].Add(soldier);
+                }
+                else if (soldierRank == 'C')
+                {
+                    if (!chowOrder.ContainsKey("Corporal"))
+                    {
+                        chowOrder.Add("Corporal", new List<string>());
+                    }
+                    chowOrder["Corporal"].Add(soldier);
+                }
+                else
+                {
+                    if (!chowOrder.ContainsKey("Private"))
+                    {
+                        chowOrder.Add("Private", new List<string>());
+                    }
+                    chowOrder["Private"].Add(soldier);
+                }
+            }
+            var output = new StringBuilder();
+            output.Append(string.Join(" ", chowOrder["Sergeant"]));
+            output.Append(" ");
+            output.Append(string.Join(" ", chowOrder["Corporal"]));
+            output.Append(" ");
+            output.Append(string.Join(" ", chowOrder["Private"]));
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
